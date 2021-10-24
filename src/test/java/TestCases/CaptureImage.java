ffc605987df8c2d6e49557d013941f0799dc8aa1
yash <maktheyashwanthrao@gmail.com>
2021-10-24 20:46:32 +0530

    comment added with Dependencies list

--- src/test/java/TestCases/CaptureImage.java
+++ src/test/java/TestCases/CaptureImage.java
@@ -20,6 +20,29 @@ public class CaptureImage {
 
 	public static void main(String[] args) throws InterruptedException, IOException {
 		
+		/*
+		 * Below Dependencies need to add in POM
+		 * <dependency>
+	     *  <groupId>ru.yandex.qatools.ashot</groupId>
+	     *  <artifactId>ashot</artifactId>
+	     *  <version>1.5.3</version>
+	     * </dependency>
+	     *   <!-- https://mvnrepository.com/artifact/com.google.code.gson/gson -->
+	     * <dependency>
+	     *   <groupId>com.google.code.gson</groupId>
+	     *   <artifactId>gson</artifactId>
+	     *   <version>2.8.8</version>
+	     * </dependency>
+	     *   <!-- https://mvnrepository.com/artifact/org.hamcrest/hamcrest-all -->
+	     * <dependency>
+	     *    <groupId>org.hamcrest</groupId>
+	     *    <artifactId>hamcrest-all</artifactId>
+	     *    <version>1.3</version>
+	     *    <scope>test</scope>
+	     * </dependency>
+		 */
+		
+		
 		System.setProperty("webdriver.chrome.driver", "./Drivers\\\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.orangehrm.com/company/about-us/about-orangehrm");
