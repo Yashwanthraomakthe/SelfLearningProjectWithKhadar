ef88683108b3c58d19a25b0009f82bc91e9666cd
yash <maktheyashwanthrao@gmail.com>
2022-01-05 12:30:53 +0530

    git practice

---
+++ src/test/java/TestCases/GitCommandsPracticeadd.java
@@ -0,0 +1,10 @@
+package TestCases;
+
+public class GitCommandsPracticeadd {
+
+	public static void main(String[] args) {
+		System.out.println("This is test class we can delete it");
+
+	}
+
+}
---
+++ src/test/java/TestCases/GitPracticeDelete.java
@@ -0,0 +1,10 @@
+package TestCases;
+
+public class GitPracticeDelete {
+
+	public static void main(String[] args) {
+		System.out.println("THis class is for delete");
+
+	}
+
+}
